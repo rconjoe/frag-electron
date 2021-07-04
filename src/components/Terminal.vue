5a05e71232c23ee0f0d89748ec6af771c4e894e1
Joe Carlton <joe@rconjoe.com>
2021-07-04 04:11:34 -0400

    init terminal

---
+++ src/components/Terminal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="xterm"> </div>
+</template>
+
+<script>
+import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
+import * as io from 'socket.io-client'
+export default {
+  data: () => ({
+  }),
+  mounted() {
+    const socket = io('ws://172.16.1.140:8080')
+    console.log(socket)
+    const term = new Terminal()
+    term.open(document.getElementById("xterm"))
+    const addon = new FitAddon()
+    term.loadAddon(addon)
+  }
+}
+</script>
+
+<style>
+
+</style>
