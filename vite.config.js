31d5be0ce3ee63d71b785da61a907c05c1aa9eaf
Joe Carlton <joe@rconjoe.com>
2021-07-25 22:10:35 -0400

    adjust base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, './dist'),
+  base: '/',
   plugins: [vue()]
 })
