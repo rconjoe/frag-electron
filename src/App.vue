bfcba94a6c357a190e71f1f91b2e0f2ef2d2e7ad
Joe Carlton <joe@rconjoe.com>
2021-07-25 22:10:52 -0400

    some server data in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
+
 <template>
-  <Tests />
+    <h2> Fragadelphia 15 </h2>
+    <h1> {{ time }} </h1>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">Home</el-breadcrumb-item>
+    </el-breadcrumb>
+  <el-container>
+    <router-view />
+  </el-container>
 </template>
 
 <script>
-import Tests from './components/Tests.vue'
-export default {
-  components: {
-    Tests,
+  export default {
+    data: () => ({
+      title: 'Overview',
+      interval: null,
+      time: null,
+      hosts: [
+        {
+          name: 'server name',
+          ip: 'http://172.16.1.140:3000'
+        }
+      ]
+    }),
+    created() {
+      // update the time every second
+      this.interval = setInterval(() => {
+        this.time = Intl.DateTimeFormat(navigator.language, {
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric'
+        }).format()
+      }, 1000)
+    },
+    beforeUnmount() {
+      clearInterval(this.interval)
+    }
   }
-}
 </script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
