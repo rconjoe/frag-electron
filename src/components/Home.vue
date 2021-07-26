a0a9cf513651619293590b7afe5f303bcd9359c4
Joe Carlton <joe@rconjoe.com>
2021-07-25 22:11:46 -0400

    add axios for express calls

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <Tests url='http://172.16.1.140:3000' />
+</template>
+
+<script>
+import Tests from './Tests.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    Tests,
+  },
+
+  data: () => ({
+  }),
+
+  mounted() {
+    this.ipTest()
+  },
+
+  methods: {
+    async ipTest() {
+      const ip = await axios.get('http://172.16.1.140:3000/hostState')
+      console.log(ip.data)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
